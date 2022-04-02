1474f9d531f280383cc1a17b9f4780dded290db5
naitickr <80129593+naitickr@users.noreply.github.com>
2022-04-02 13:57:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,68 @@
-var prediction_1="";
-var prediction_2="";
-  
+var prediction_1 = "";
+var prediction_2 = "";
+
 
 Webcam.set({
-    width:350,
-     height:300, 
-     image_format : 'png', 
-     png_quality:90 }); 
-    camera = document.getElementById("camera"); 
-    Webcam.attach('#camera');
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
 
-    function takesnapshot()
-    {
-    Webcam.snap(function(data_uri){
-document.getElementById("result").innerHTML= '<img id="captured_image" src=' + data_uri + '"/>'
-    }
-    );
-    }
-console.log('ml5.version',ml5.version);
- 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/A34KVJBGw/model.json',modelLoaded);
-    
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src=' + data_uri + '"/>'
+    });
+}
+console.log('ml5.version', ml5.version);
 
-function modelLoaded() 
-{ console.log('ModelLoaded!'); 
+classifier = ml5.imageClassifier('model.json', modelLoaded);
 
-function speak(){ var synth = window.speechSynthesis; speak_data_1 = "The first prediction is " + prediction_1; speak_data_2 = "And the second prediction is " + prediction_2; var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2); synth.speak(utterThis);}
+
+function modelLoaded() {
+    console.log('ModelLoaded!');
+
+    function speak() {
+        var synth = window.speechSynthesis;
+        speak_data_1 = "The first prediction is " + prediction_1;
+        speak_data_2 = "And the second prediction is " + prediction_2;
+        var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+        synth.speak(utterThis);
+
+        function check() {
+            img = document.getElementById('captured_image');
+            classifier.classify(img, gotResult);
+        }
+
+        function gotResult(error, results) {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(results);
+                document.getElementById("result_finger_name").innerHTML = results[0].label;
+                document.getElementById("result_finger_name2").innerHTML = results[1].label;
+                prediction_1 = results[0].label;
+                prediction_2 = results[1].label;
+                speak();
+                if (results[0].label == "victory") {
+                    document.getElementById("update_finger").innerHTML = "&#128522;"
+                }
+                if (results[0].label == "amazing") {
+                    document.getElementById("update_finger").innerHTML = "&#128532;"
+                }
+                if (results[0].label == "best") {
+                    document.getElementById("update_finger").innerHTML = "&#128548;"
+                }
+                if (results[1].label == "victory") {
+                    document.getElementById("update_finger2").innerHTML = "&#128522;"
+                }
+                if (results[1].label == "amazing") {
+                    document.getElementById("update_finger2").innerHTML = "&#128532;"
+                }
+                if (results[1].label == "best") {
+                    document.getElementById("update_finger2").innerHTML = "&#128548;"
+                }
+            }
+        }
